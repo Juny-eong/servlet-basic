8285d861cd7947b4bafdcb5d1b5fb21f503a9e0b
juny-eong <adipy1470@naver.com>
2021-04-28 20:16:34 +0900

    [Q1152] count token

---
+++ etc/src/implementation/Q1152.java
@@ -0,0 +1,19 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1152 {
+	
+	public static void main(String[] args) throws Throwable {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.println(st.countTokens());
+		
+	}
+
+}
