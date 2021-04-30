330e5a8d0f8220df9c723e22422d731429b9a4ad
junyeong-_-lim <junyeong_lim@tmax.co.kr>
2021-04-30 20:07:35 +0900

    [Q14500] tetromino

--- test/src/bruteForce/Q14500.java
+++ test/src/bruteForce/Q14500.java
@@ -6,10 +6,13 @@ import java.util.StringTokenizer;
 
 public class Q14500 {
 	
-	public static int[][] map;
-	public static int N;
-	public static int M;
-	public static int count = 0;
+	private static boolean[][] visited;
+	private static int[][] map;
+	private static int[] dx = { -1, 0, 1, 0 };
+	private static int[] dy = { 0, -1, 0, 1 };
+	private static int N;
+	private static int M;
+	private static int max;
 	
 	public static void main(String[] args) throws Throwable {
 		
@@ -21,8 +24,9 @@ public class Q14500 {
 		M = Integer.parseInt(st.nextToken());
 		
 		map = new int[N][M];
-		int maxValue = 0;
-		
+		visited = new boolean[N][M];
+		max = 0;
+
 		/* input으로 맵 생성 */
 		for (int i = 0; i < N; i++) {
 			
@@ -36,12 +40,63 @@ public class Q14500 {
 			
 			for (int x = 0; x < M; x++) {
 				
-				for (int i = 0; i < 4; i++) {
-//					maxValue = map[getMaxValue(map, y, x)[0]][getMaxValue(map, y, x)[0]];
-					
-				}
+				DFS(y, x, 0, 0);
+				Exception(y, x);
 			}
 		}
+		System.out.println(max);
+	}
+	
+	public static void DFS(int x, int y, int depth, int sum) {
+		
+		if (depth == 4) {
+			max = Math.max(max, sum);
+			return;
+		}
+		
+		/* 상하좌우로 움직인다. */
+		for (int i = 0; i < 4; i++) {
+			int nextX = x + dx[i];
+			int nextY = y + dy[i];
+			
+			/* 범위를 벗어나는 경우 제외 */
+			if (nextX < 0 || nextY < 0 || nextX >=N || nextY >= M) 
+				continue;
+			
+			/* 이미 방문한 경로 제외 */
+			if (visited[nextX][nextY]) 
+				continue;
+			
+			visited[nextX][nextY] = true;
+			DFS(nextX, nextY, depth + 1, sum + map[nextX][nextY]);
+			visited[nextX][nextY] = false; // 다시 초기화해준다.
+		}
+		
+	}
+	
+	public static void Exception(int x, int y) {
+		int wing = 4;
+		int min = Integer.MAX_VALUE;
+		int sum = map[x][y];
+		
+		for (int i = 0; i < 4; i++) {
+			int nextX = x + dx[i];
+			int nextY = y + dy[i];
+			
+			if (wing <= 2) return;
+			
+			if (nextX < 0 || nextY < 0 || nextX >=N || nextY >= M) {
+				wing--;
+				continue;
+			}
+			min = Math.min(min,  map[nextX][nextY]);
+			sum = sum + map[nextX][nextY];
+		}
+		
+		if (wing ==4) {
+			sum = sum - min;
+		}
+		max = Math.max(max, sum);
 	}
 	
 	public static int[] getMaxValue(int[][] map, int y, int x) {
