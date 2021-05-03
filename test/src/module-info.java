61ba528d57e27a224be749c18740cd41974e2d7f
juny-eong <adipy1470@naver.com>
2021-05-03 14:47:14 +0900

    add module-info

---
+++ test/src/module-info.java
@@ -0,0 +1,8 @@
+module test {
+	exports algorithm.dynamicProgramming;
+	exports algorithm.implementation;
+	exports algorithm.bruteForce;
+	exports algorithm.mathematics;
+	exports basic;
+	exports algorithm.dataStructure;
+}
