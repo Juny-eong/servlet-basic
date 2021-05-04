230a53445b44887d3f75534e9b82b2a9a3c80342
juny-eong <adipy1470@naver.com>
2021-05-04 15:20:36 +0900

    Kruskal-Algorithm example

---
+++ test/src/algorithm/graphsearch/kruskal/Edge.java
@@ -0,0 +1,29 @@
+package algorithm.graphsearch.kruskal;
+
+public class Edge implements Comparable<Edge> {
+	
+	int v1;
+	int v2;
+	int cost;
+	
+	Edge(int v1, int v2, int cost) {
+		this.v1 = v1;
+		this.v2 = v2;
+		this.cost = cost;
+	}
+	
+	@Override
+	public int compareTo(Edge o) {
+		if (this.cost < o.cost) {
+			return -1;
+		}
+		else if (this.cost == o.cost) {
+			return 0;
+		}
+		else
+			return 1;
+	}
+	
+	
+
+}
---
+++ test/src/algorithm/graphsearch/kruskal/Main.java
@@ -0,0 +1,77 @@
+package algorithm.graphsearch.kruskal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+	public static int[] parent;
+	public static ArrayList<Edge> edgeList;
+	
+	
+	public static void main(String[] args) {
+		
+		edgeList = new ArrayList<Edge>();
+		edgeList.add(new Edge(1, 4, 4));
+        edgeList.add(new Edge(1, 2, 6));
+        edgeList.add(new Edge(2, 3, 5));
+        edgeList.add(new Edge(2, 4, 3));
+        edgeList.add(new Edge(2, 5, 7));
+        edgeList.add(new Edge(2, 6, 8));
+        edgeList.add(new Edge(3, 6, 8));
+        edgeList.add(new Edge(4, 5, 9));
+        edgeList.add(new Edge(5, 6, 11));
+        
+        parent = new int[7]; // node list
+        for (int i = 1; i <= 6; i++) {
+        	parent[i] = i;
+        }
+        
+        Collections.sort(edgeList);
+        
+        int sum = 0;
+        
+        
+        for (int i = 0; i < edgeList.size(); i++) {
+        	Edge edge = edgeList.get(i);
+        	
+        	/* 서로 다른 집합이면 그 코스트를 더하고 union 한다. */
+        	if (!isSameParent(edge.v1, edge.v2)) {
+        		sum += edge.cost;
+        		union(edge.v1, edge.v2);
+        	}
+        }
+        System.out.println(sum);
+        
+	}
+	
+	
+	
+	public static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		
+		if (x != y) {
+			parent[y] = x;
+		}
+	}
+	
+	
+	public static int find(int x) {
+		if (parent[x] == x) {
+			return x;
+		}
+		return parent[x] = find(parent[x]);
+	}
+	
+	
+	public static boolean isSameParent(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if (x == y) 
+			return true;
+		else 
+			return false;
+	}
+	
+}
