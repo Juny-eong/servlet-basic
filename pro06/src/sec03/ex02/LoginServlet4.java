38cec5421ec7353234135f4f5e6e23b39227e77a
juny-eong <adipy1470@naver.com>
2021-05-15 18:15:12 +0900

    doHandle method test

---
+++ pro06/src/sec03/ex02/LoginServlet4.java
@@ -0,0 +1,58 @@
+package sec03.ex02;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet4
+ */
+@WebServlet("/login4")
+public class LoginServlet4 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init method.");
+	}
+
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("doGet method.");
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("doPost method.");
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("doHandle method.");
+		
+		request.setCharacterEncoding("utf-8");
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		System.out.println("user_id : " + user_id);
+		System.out.println("user_pw : " + user_pw);
+		
+		
+	}
+
+}
