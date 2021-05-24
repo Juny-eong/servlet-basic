18cf95785c83840e3c999dc6583c13c3ab839c84
juny-eong <adipy1470@naver.com>
2021-05-25 00:56:56 +0900

    add LoginServlet5.java

--- pro06/src/sec03/ex03/LoginServlet5.java
+++ pro06/src/sec03/ex03/LoginServlet5.java
@@ -1,6 +1,8 @@
 package sec03.ex03;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,30 +21,43 @@ public class LoginServlet5 extends HttpServlet {
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		System.out.println("init method.");
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		String address = request.getParameter("user_address");
+		
+		System.out.println("id : " + id);
+		System.out.println("pw : " + pw);
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "id : " + id + "<br>";
+		data += "pw : " + pw + "<br>";
+		data += "address : " + address;
+		data += "</body>";
+		data += "</html>";
+		
+		out.print(data);
+		
 	}
 
 	/**
 	 * @see Servlet#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("destroy method.");
 	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+	
 }
