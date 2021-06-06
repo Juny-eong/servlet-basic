a16bc7b476f0bddb0acf39e237caf8c0c3f287fd
juny-eong <adipy1470@naver.com>
2021-06-06 14:24:53 +0900

    code update2

--- pro07/src/sec02/ex02/MemberDAO.java
+++ pro07/src/sec02/ex02/MemberDAO.java
@@ -12,25 +12,31 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 public class MemberDAO {
-	
+
 	private Connection con;
 	private PreparedStatement pstmt;
 	private DataSource dataFactory;
 
-	
 	public MemberDAO() {
-		
+
+		/* JNDI access (default path=java:/comp/env) */
 		try {
+			
 			Context ctx = new InitialContext();
 			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			
+			/* context.xml resource-name */
 			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	public List<MemberVO> listMembers() {
-		List<MemberVO> list = new ArrayList<MemberVO>();
+		
+		List<MemberVO> list = new ArrayList<>();
+		
 		try {
 			// connDB();
 			con = dataFactory.getConnection();
@@ -38,12 +44,14 @@ public class MemberDAO {
 			System.out.println("prepareStatememt: " + query);
 			pstmt = con.prepareStatement(query);
 			ResultSet rs = pstmt.executeQuery();
+			
 			while (rs.next()) {
 				String id = rs.getString("id");
 				String pwd = rs.getString("pwd");
 				String name = rs.getString("name");
 				String email = rs.getString("email");
 				Date joinDate = rs.getDate("joinDate");
+				
 				MemberVO vo = new MemberVO();
 				vo.setId(id);
 				vo.setPwd(pwd);
@@ -55,23 +63,29 @@ public class MemberDAO {
 			rs.close();
 			pstmt.close();
 			con.close();
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		return list;
 	}
 
+	
 	public void addMember(MemberVO memberVO) {
+		
 		try {
 			con = dataFactory.getConnection();
 			String id = memberVO.getId();
 			String pwd = memberVO.getPwd();
 			String name = memberVO.getName();
 			String email = memberVO.getEmail();
+			
 			String query = "insert into t_member";
 			query += " (id,pwd,name,email)";
 			query += " values(?,?,?,?)";
 			System.out.println("prepareStatememt: " + query);
+			
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, id);
 			pstmt.setString(2, pwd);
@@ -79,21 +93,27 @@ public class MemberDAO {
 			pstmt.setString(4, email);
 			pstmt.executeUpdate();
 			pstmt.close();
-		} catch (Exception e) {
+			
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	
 	public void delMember(String id) {
+		
 		try {
 			con = dataFactory.getConnection();
 			String query = "delete from t_member" + " where id=?";
 			System.out.println("prepareStatememt:" + query);
+			
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, id);
 			pstmt.executeUpdate();
 			pstmt.close();
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
