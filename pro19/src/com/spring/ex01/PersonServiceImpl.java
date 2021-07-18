716ab3664d3fe61a0d320a6d001a767dd88ea87e
juny-eong <adipy1470@naver.com>
2021-07-18 15:35:00 +0900

    add new project

---
+++ pro19/src/com/spring/ex01/PersonServiceImpl.java
@@ -0,0 +1,20 @@
+package com.spring.ex01;
+
+public class PersonServiceImpl implements PersonService {
+	
+	private String name;
+	private int age;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		System.out.println("name: " + name);
+		System.out.println("age: " + age);
+	}
+	
+	
+}
